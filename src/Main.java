666686aadebe8052f6a1d63e96578f689eb81b87
peterkuruc <kpetko@gmail.com>
2023-06-26 13:38:38 +0200

    c3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         greet("name");
     }
-    public static void greet(String name){
-        System.out.println("Hello" + name);
+    public static void greet(){
+        System.out.println("Greetings");
     }
 
     public void count(Integer i) {
