7b001155b3d4558d96b4b661998a90fb5ff47f79
Lopaticcka <ondrakutis@seznam.cz>
2023-06-26 13:47:59 +0200

    b4

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,11 @@ public class Main {
         System.out.println("Greetings");
     }
 
-
     public void count() {
         System.out.println("9");
 
-
-<<<<<<< HEAD
-    public void cheer(String name) {
-        System.out.println(name);
-=======
     public void cheer() {
         System.out.println("JAVA");
->>>>>>> 6cf37f02ace79dfe86b7a8407c229b2894cfb5e4
     }
 }
 
