3acc6ef6aa40794f83952be1ffeb777bbcceb38f
Lopaticcka <ondrakutis@seznam.cz>
2023-06-26 14:42:08 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
     }
-    public static void greet(String name){
-        System.out.println("Greetings" + name);
+    public static void greet(String name, String age){
+        System.out.println("Greetings " + name + "age " + age);
     }
 
     public void count(String param1, String param2) {
