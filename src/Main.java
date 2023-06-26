c0e2b5fcfd89793359ec1f8e4a7fd29b69775d33
Stofi09 <kristof.solak@gmail.com>
2023-06-26 13:15:22 +0200

    a1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        greet();
+    }
+    public static void greet(){
+        System.out.println("Hello Kristof");
     }
 }
