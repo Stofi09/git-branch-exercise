64d655df1e41b7bebcf6756be26f81866182f78b
Stofi09 <kristof.solak@gmail.com>
2023-06-26 14:40:49 +0200

    a6

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
         System.out.println("9");
     }
 
-    public void cheer() {
-        System.out.println("JAVA");
+    public void cheer(String cheer) {
+        System.out.println("Cheer for the" + cheer);
     }
 }
 
