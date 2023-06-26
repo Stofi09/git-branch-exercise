7d41c1642faa70c3c940485745bb74c432c5d2f4
Lopaticcka <ondrakutis@seznam.cz>
2023-06-26 14:01:22 +0200

    b5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        greet();
     }
     public static void greet(String name){
         System.out.println("Greetings" + name);
     }
 
-    public void count() {
-        System.out.println("9");
+    public void count(String param1, String param2) {
+        System.out.println(param1);
+        System.out.println(param2);
     }
 
     public void cheer() {
